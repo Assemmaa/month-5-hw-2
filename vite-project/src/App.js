89b719787bfcdcce4f7e9de775b83923d6582d08
Assemmaa <asemaibraeva88@gmail.com>
2023-06-27 13:44:37 +0600

    first commit

---
+++ vite-project/src/App.js
@@ -0,0 +1,27 @@
+export default App
+
+import './App.css'
+import {Route, Routes} from "react-router-dom";
+import PostList from "./Pages/PostList/PostList";
+import DeletePostConfirmation from "./Pages/DeletePostConfirmation/DeletePostConfirmation.js";
+import EditPostForm from "./Pages/EditPostForm/EditPostForm";
+import CreatePostForm from "./Pages/CreatePostForm/CreatePostForm.js";
+import Header from "./components/Header.js";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path='/' element={<PostList />} />
+                <Route path='delete' element={<DeletePostConfirmation />} />
+                <Route path='edit' element={<EditPostForm />} />
+                <Route path='create' element={<CreatePostForm/>} />
+
+
+            </Routes>
+        </>
+    )
+}
+
+export default App
